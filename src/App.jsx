167777ae63a2adbabbb9575198711ac6b847b459
vikramveeran <vikram.shanthipura@gmail.com>
2025-09-18 13:38:01 +0530

    git commiting

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Box from './Box'
+import Introdunction from './Introdunction'
+
+const App = () => {
+   return <Introdunction/>
+}
+export default App
---
+++ src/Box.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='w-2.5 h-2.5 bg-amber-600'></div>
+  )
+}
+
+export default Box
