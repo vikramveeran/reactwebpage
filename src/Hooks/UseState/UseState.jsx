0cd6f476c116df0fdd075b584ad6682e4b58baf4
vikramveeran <vikram.shanthipura@gmail.com>
2026-01-14 18:04:43 +0530

    batch update

--- src/Hooks/UseState/UseState.jsx
+++ src/Hooks/UseState/UseState.jsx
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 
 const UseState = () => {
   const[num,setNum] =  useState(10)
-   const changeNum = () =>{
+   const Increase = () =>{
        
-      setNum(num => num+1)
+      setNum(prev => (prev+1))
+       setNum(prev => (prev+1))
+        setNum(prev => (prev+1))
+   }
+   const Decrease = () =>{
+     setNum(num-1)
    }
    
   return (
-    <div>
+    <div className='mr-3'>
       <h1>value of num is {num}</h1>
-      <button className='bg-blue-300' onClick={changeNum}>Click here</button>
+      <button className='bg-red-300 ' onClick={Increase}>Increase</button>
+      {/* <button className='bg-blue-300 relative -right-2.5' onClick={Decrease}>DE</button> */}
     </div>
   )
 }
